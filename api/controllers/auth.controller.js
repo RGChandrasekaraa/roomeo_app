a179380ee4f2b62d23e46e40262b473783a17898
Chandrasekaraa Ramadoss Ganesh <94929823+RGChandrasekaraa@users.noreply.github.com>
2024-04-29 21:03:09 +1000

    login, auth modified

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'; 
 import prisma from '../lib/prisma.js';
 
 export const register = async (req, res) => {
@@ -26,9 +27,11 @@ export const register = async (req, res) => {
 }
 
 export const login = async (req, res) => {
+    console.log("Login request received", req.body);
     const { username, password } = req.body;
     
     try {
+        const age = (1000 * 60 * 60 * 24 * 7)
         // Find user in database
         const user = await prisma.user.findUnique({
             where: { username: username }
@@ -38,22 +41,35 @@ export const login = async (req, res) => {
             return res.status(401).json({ message: 'invalid credential ' });
         }
         // Check if password is valid
-        const isPasswordValid = await bcrypt.compare(password, user.password);  
+        const isPasswordValid = await bcrypt.compare(password, user.password);
         if (!isPasswordValid) {
             return res.status(401).json({ message: 'invalid credential ' });
         }
-         
-
-
+        
+        // Generate token
+        const token = jwt.sign({
+            id: user.id,
+        },process.env.JWT_SECRET_KEY, { expiresIn: age }); 
+        // cookie
+        res.cookie("token ", token, {
+            httpOnly: true,
+            // secure: true,
+            maxAge: age,  
+        }).status(200).json({ message: 'Logged in' });
     }
-
 
     catch (error) {
         console.log(error)
         res.status(500).json({ message: 'Error logging in' });
     }
-    } 
+}; 
 
 export const logout = (req, res) => {
-    res.send('Logout page');
+    try {
+        res.clearCookie('token').status(200).json({ message: 'Logged out' });
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Error logging out' });
+    }
 }  
--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -1,15 +1,46 @@
+import { useState } from "react";
 import "./login.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false); // [1  
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true); // [2  
+    const formData = new FormData(e.target);
+    
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    try {
+      const response = await axios.post("http://localhost:8800/api/auth/login", {
+        username,
+        email,
+        password
+      });
+      navigate("/n "); 
+    }
+    catch (err) {
+      console.log(err);
+      setError(err.response.data.message); 
+    }
+    finally
+    {
+      setIsLoading(false); // [3  
+    }
+  };
   return (
     <div className="login">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Welcome back</h1>
-          <input name="username" type="text" placeholder="Username" />
+          <input name="username" required minLength={3} maxLength={20 } type="text" placeholder="Username" />
           <input name="password" type="password" placeholder="Password" />
-          <button>Login</button>
+          <button disabled={isLoading} >Login</button>
+          {error && <span className="error">{error}</span>}
           <Link to="/register">{"Don't"} you have an account?</Link>
         </form>
       </div>
